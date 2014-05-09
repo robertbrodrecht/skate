fa539de601ab37033558db75a7a449c5b34877ef
Robert Brodrecht <robert@bigcom.com>
2014-05-09 11:15:58 -0500

    Syncing up

--- js/custom.js
+++ js/custom.js
@@ -2,7 +2,7 @@
 //$.fn.skate.defaults.debug = true;
 $.fn.skate.defaults.css = location.href.indexOf('js-only') > -1 ? false : true;
 //$.fn.skate.defaults.transition = 2;
-$.fn.skate.defaults.createnav = 'merge';
+//$.fn.skate.defaults.createnav = 'none';
 
 $('.no-animation .skate').skate({animation: 'none', 'keyboard': false});
 cfskate = $('.crossfade-animation .skate').skate({animation: 'crossfade'});
